9587c34345d8b914a5f3532b0a193fa2f306ec32
S1kerAce <yeojb2014@yandex.ru>
2025-05-06 22:30:57 +0400

    Colors+opt

--- app/src/main/java/com/example/catchat/MainActivity.kt
+++ app/src/main/java/com/example/catchat/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.catchat
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -16,48 +18,56 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
-        // Дает команду Android рассматривать панель инструментов так,
-        // словно она является стандартной панелью приложения, и вывести на ней имя приложения
         setSupportActionBar(toolbar)
 
-        // получаем ссылку на контроллер навигации из хоста навигации
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // связывание выдвижной панели с контроллером навигации
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         val navView = findViewById<NavigationView>(R.id.nav_view)
         NavigationUI.setupWithNavController(navView, navController)
 
-        // Связь панели инструментов с графом навигации
-        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val builder = AppBarConfiguration.Builder(navController.graph)
-        // на панели инструментов выводится значок для открытия выдвижной панели
-        builder.setOpenableLayout(drawer)
-        val appBarConfiguration = builder.build()
+        appBarConfiguration = AppBarConfiguration.Builder(
+            navController.graph
+        ).setOpenableLayout(drawer).build()
+
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
-        // связываем нижнюю панель навигации с контроллером навигации
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavView.setupWithNavController(navController)
 
+        findViewById<BottomNavigationView>(R.id.bottom_nav)?.apply {
+            setupWithNavController(navController)
+
+            val states = ColorStateList(
+                arrayOf(
+                    intArrayOf(android.R.attr.state_checked),  // активный
+                    intArrayOf(-android.R.attr.state_checked)  // неактивный
+                ),
+                intArrayOf(
+                    ContextCompat.getColor(this@MainActivity, R.color.blue_active),
+                    ContextCompat.getColor(this@MainActivity, R.color.red_inactive)
+                )
+            )
+            itemIconTintList = states
+            itemTextColor = states
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
-        return super.onCreateOptionsMenu(menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return item.onNavDestinationSelected(navController)
-                || super.onOptionsItemSelected(item)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
-
 }
